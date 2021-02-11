7acdceaa4a774dfafc1bad3ae81dcb78e7c3eca5
DevDouglas1 <61925933+DevDouglas1@users.noreply.github.com>
2021-02-11 13:41:56 -0300

    finalizando backend

--- backend/tests/integration/ong.spec.js
+++ backend/tests/integration/ong.spec.js
@@ -4,6 +4,7 @@ const connection = require('../../src/database/connection');
 
 describe('ONG', () => {
     beforeEach(async () => {
+        await connection.migrate.rollback();
         await connection.migrate.latest();
     });
 
@@ -12,7 +13,8 @@ describe('ONG', () => {
     });
 
     it('should be able to create a new ONG', async () => {
-        const response = await request(app).post('/ongs')
+        const response = await request(app)
+        .post('/ongs')
         .send({
             name: "APAD",
             email: "contato@teste.com",
